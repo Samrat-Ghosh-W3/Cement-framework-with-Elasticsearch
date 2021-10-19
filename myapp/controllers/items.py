9e06a1a9a3cddf5909921809bb841c0321adbfb3
Samrat-Ghosh-W3 <samrat@w3engineers.com>
2021-10-19 14:05:47 +0600

    first commit

---
+++ myapp/controllers/items.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from cement import Controller, ex
+
+
+es = Elasticsearch()
+
+
+class Items(Controller):
+    class Meta:
+        label = "items"
+        stacked_type = "embedded"
+        stacked_on = "base"
+
+    @ex(help="list items")
+    def list(self):
+        output = es.get(index="test-my-index", doc_type="my-test-type", id=1)
+        print(output["_source"])
+
+    @ex(help="create new item")
+    def create(self):
+        from .services import addIndex
+
+        query = addIndex()
+        es.index(index="test-my-index", doc_type="my-test-type", id=1, document=query)
+        print("Index is created.")
+
+    @ex(help="update an existing item")
+    def update(self):
+        from .services import updateIndex
+
+        query = updateIndex()
+        es.update(index="test-my-index", doc_type="my-test-type", id=1, document=query)
+        print("Index is updated")
+
+    @ex(help="delete an item")
+    def delete(self):
+        es.delete(index="test-my-index", id=1)
+        print("Index deleted Successfuly")
+
+    @ex(help="complete an item")
+    def complete(self):
+        print("Completed")
---
+++ myapp/controllers/services.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+
+def addIndex():
+    data = {
+        "f-name": "samrat",
+        "l-name": "ghosh",
+        "company": "w3engineers",
+        "Branch": "Khulna",
+        "timestamp": datetime.now()
+    }
+    return data
+
+
+def updateIndex():
+    data = {
+       "f-name": "samrat",
+        "l-name": "ghosh",
+        "company": "w3engineers",
+        "Branch": "Dhaka",
+        "timestamp": datetime.now()
+    }
+    return data
